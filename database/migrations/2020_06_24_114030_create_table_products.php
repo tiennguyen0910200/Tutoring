6f9750a0652b268b81882fc1ea6dffb799aa053e
Nguyen Thi Tien <tien.nguyen21@pn.com.vn>
2020-06-24 19:34:42 +0700

    finish migration

---
+++ database/migrations/2020_06_24_114030_create_table_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->double('origin_price');
+            $table->double('discount_price');
+            $table->integer('quantity');
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
